67ee29b62a63967302a0bd57609c21c2a10dca1e
Ismael Gaudioso <ismaelgaudioso@gmail.com>
2022-04-05 14:43:43 +0200

    mostrando/ocultado overlay

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from 'react';
-import './Tab1.css';
+
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonViewDidEnter, isPlatform, IonCardSubtitle, IonNote, IonFab, IonFabButton, IonIcon, IonBadge, IonRow, IonCol, IonButton } from '@ionic/react';
+import { list } from 'ionicons/icons';
+
 import { Map, Marker, Overlay } from "pigeon-maps";
 import { maptiler } from 'pigeon-maps/providers';
-import { flashOffOutline, flashOutline, list, arrowForward, call, callOutline, navigateOutline, phoneLandscapeOutline, phonePortraitOutline, pricetag, pricetags, pricetagsOutline } from 'ionicons/icons';
+
 import { MapOverlay } from '../components/MapOverlay';
+import { CurrentPointOverlay } from '../components/CurrentPointOverlay';
+
+import './Tab1.css';
+
+
 
 
 const maptilerProvider = maptiler('rQmJ3tL4fQSG3TUeAarU', 'streets');
@@ -12,7 +19,11 @@ const maptilerProvider = maptiler('rQmJ3tL4fQSG3TUeAarU', 'streets');
 const Tab1: React.FC = () => {
 
   const [currentPoint, setCurrentPoint] = useState({ latitude: 41.580833, longitude: -1.116111 });
+  const [showCurrentPointInfo, setShowCurrentPointInfo] = useState(false);
+
   const [markerPosition] = useState({ latitude: 41.578633, longitude: -1.114111 });
+  const [results, setResults] = useState(false);
+
   const web = isPlatform("desktop" || "pwa" || "mobileweb" || "");
 
 
@@ -20,9 +31,21 @@ const Tab1: React.FC = () => {
     window.dispatchEvent(new Event('resize'));
   });
 
-  const handleShowCurrentPointInfo = () => {
-    alert("moco");
+  const hideMarkers = () => {
+    setResults(false);
+    setShowCurrentPointInfo(false);
+  }
 
+  const showMarkerInfo = () => {
+
+    setShowCurrentPointInfo(!showCurrentPointInfo);
+    setResults(!results);
+
+  }
+
+  const handleShowCurrentPointInfo = () => {
+    hideMarkers();
+    setShowCurrentPointInfo(!showCurrentPointInfo);
   }
 
   return (
@@ -53,29 +76,23 @@ const Tab1: React.FC = () => {
               width={50}
               anchor={[currentPoint.latitude, currentPoint.longitude]}
             />
-            <Overlay anchor={[currentPoint.latitude, currentPoint.longitude]} offset={[95, 182]}>
-              <div className="overlayContainer">
-
-                <IonCardSubtitle>Current Location</IonCardSubtitle>
-                <IonNote color="medium">Click on the lightning button then choose a new point on the map to view places around that point.</IonNote>
-              </div>
-            </Overlay>
+            {showCurrentPointInfo &&
+              <Overlay anchor={[currentPoint.latitude, currentPoint.longitude]} offset={[95, 140]}>
+                <CurrentPointOverlay />
+              </Overlay>
+            }
 
 
-
-
-
-
-            <Marker onClick={handleShowCurrentPointInfo}
+            <Marker onClick={showMarkerInfo}
               key={0}
               color="#3578e5"
               width={40}
               anchor={[markerPosition.latitude, markerPosition.longitude]} />
-
-            <Overlay anchor={[markerPosition.latitude, markerPosition.longitude]} offset={[95, 210]}>
-              <MapOverlay />
-            </Overlay>
-
+            {results &&
+              <Overlay anchor={[markerPosition.latitude, markerPosition.longitude]} offset={[95, 210]}>
+                <MapOverlay />
+              </Overlay>
+            }
 
 
 
